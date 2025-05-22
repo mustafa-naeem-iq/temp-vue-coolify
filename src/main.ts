790db0fc2c4bfd125b82ee54904913151734b874
mustafa-naeem <mustafa.naeem.iq@gmail.com>
2025-05-22 19:16:13 +0300

    one

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="super counter" type="button"></button>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
